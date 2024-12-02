53609e90bab42863babcced1ee8862e481bb8d38
SoraHrS <soherreras@unal.edu.co>
2024-12-02 14:30:56 -0500

    se realizó el ejercicio 19

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double lado, perimetro, altura, area;
+        Scanner sc = new Scanner(System.in);
+        sc.useLocale(Locale.US);
+        System.out.println("Ingrese el valor del lado del triangulo equilatero: ");
+        lado = sc.nextDouble();
+
+        perimetro = lado * 3;
+        altura = (lado * Math.sqrt(3))/2;
+        area = (lado * altura)/2;
+
+        System.out.println("El perimetro es: " + perimetro);
+        System.out.println("El altura es: " + altura);
+        System.out.println("El area del triangulo es: " + area);
     }
 }
